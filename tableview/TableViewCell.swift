086c0fbee1ab86ff8ea4fe3dce431a1aeee7f69b
jack <yrw970127@gmail.com>
2021-02-16 16:49:22 -0800

    commit

---
+++ tableview/TableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewCell.swift
+//  tableview
+//
+//  Created by Jack Yu on 2/16/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblcity: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
